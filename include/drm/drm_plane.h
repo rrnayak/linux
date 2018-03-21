b4c9b66be58a02ada8bf39726f27ec56cbc67c2e
Peter Ujfalusi <peter.ujfalusi@ti.com>
2018-03-21 12:20:24 +0200

    UPSTREAM: drm: Add drm_mode_config->normalize_zpos boolean
    Instead of drivers duplicating the drm_atomic_helper_check() code to be
    able to normalize the zpos they can use the normalize_zpos flag to let the
    drm core to do it.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>
    Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20180321102029.15248-2-peter.ujfalusi@ti.com
    (cherry picked from commit 49efffc7fbd48d5ea3d0dd60c218c7502d4a179d)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    BUG=b:109756482
    TEST=Boots on cheza (with sandbox patches on top)
    
    Change-Id: Id7b3ed0dd54864060e7d3f55ba6e242eaf7789a9
    Reviewed-on: https://chromium-review.googlesource.com/1158885
    Commit-Ready: Sean Paul <seanpaul@chromium.org>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Kristian H. Kristensen <hoegsberg@chromium.org>

--- include/drm/drm_plane.h
+++ include/drm/drm_plane.h
@@ -34,12 +34,28 @@ struct drm_modeset_acquire_ctx;
 
 /**
  * struct drm_plane_state - mutable plane state
- *
- * Please not that the destination coordinates @crtc_x, @crtc_y, @crtc_h and
- * @crtc_w and the source coordinates @src_x, @src_y, @src_h and @src_w are the
- * raw coordinates provided by userspace. Drivers should use
- * drm_atomic_helper_check_plane_state() and only use the derived rectangles in
- * @src and @dst to program the hardware.
+ * @plane: backpointer to the plane
+ * @crtc_w: width of visible portion of plane on crtc
+ * @crtc_h: height of visible portion of plane on crtc
+ * @src_x: left position of visible portion of plane within
+ *	plane (in 16.16)
+ * @src_y: upper position of visible portion of plane within
+ *	plane (in 16.16)
+ * @src_w: width of visible portion of plane (in 16.16)
+ * @src_h: height of visible portion of plane (in 16.16)
+ * @rotation: rotation of the plane
+ * @zpos: priority of the given plane on crtc (optional)
+ *	Note that multiple active planes on the same crtc can have an identical
+ *	zpos value. The rule to solving the conflict is to compare the plane
+ *	object IDs; the plane with a higher ID must be stacked on top of a
+ *	plane with a lower ID.
+ * @normalized_zpos: normalized value of zpos: unique, range from 0 to N-1
+ *	where N is the number of active planes for given crtc. Note that
+ *	the driver must set drm_mode_config.normalize_zpos or call
+ *	drm_atomic_normalize_zpos() to update this before it can be trusted.
+ * @src: clipped source coordinates of the plane (in 16.16)
+ * @dst: clipped destination coordinates of the plane
+ * @state: backpointer to global drm_atomic_state
  */
 struct drm_plane_state {
 	/** @plane: backpointer to the plane */
