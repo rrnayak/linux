7cfa0bad2592be4cf16914d2005f869c30297549
Sharat Masetty <smasetty@codeaurora.org>
2019-08-29 18:13:51 +0530

    drm: msm: a6xx: Add support for A618
    This patch adds support for enabling Graphics Bus Interface(GBIF)
    used in multiple A6xx series chipets. Also makes changes to the
    PDC/RSC sequencing specifically required for A618. This is needed
    for proper interfacing with RPMH.
    
    Change-Id: I0e7a314b0ae0d562f602512ad978f5d1ced5fb26
    Signed-off-by: Sharat Masetty <smasetty@codeaurora.org>
    Signed-off-by: Atul Dhudase <adhudase@codeaurora.org>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.h
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.h
@@ -216,6 +216,16 @@ static inline int adreno_is_a540(struct adreno_gpu *gpu)
 	return gpu->revn == 540;
 }
 
+static inline int adreno_is_a618(struct adreno_gpu *gpu)
+{
+       return gpu->revn == 618;
+}
+
+static inline int adreno_is_a630(struct adreno_gpu *gpu)
+{
+       return gpu->revn == 630;
+}
+
 int adreno_get_param(struct msm_gpu *gpu, uint32_t param, uint64_t *value);
 const struct firmware *adreno_request_fw(struct adreno_gpu *adreno_gpu,
 		const char *fwname);
